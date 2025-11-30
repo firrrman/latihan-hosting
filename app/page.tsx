08669a265ffafe2a1b369356290e7a687eee05ff
firrrman <firmanhakim11704@gmail.com>
2025-11-30 10:20:04 +0700

    test update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const users = await getUsers();
   return (
     <div className="p-8 max-w-3xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6">CRUD User + Post</h1>
+      <h1 className="text-3xl font-bold mb-6">CRUD USERRRRRRRR</h1>
 
       {/* LIST USER */}
       <div className="space-y-6">
